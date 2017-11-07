7f888c7c3a10fb940cf2838ea74e5efb15177ea1
Todd Williams <twilliams0459@gmail.com>
2017-11-07 04:06:05 -0500

    DomoMaker C

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,11 +3,11 @@ const models = require('../models');
 const Account = models.Account;
 
 const loginPage = (req, res) => {
-  res.render('login');
+  res.render('login', { csrfToken: req.csrfToken() });
 };
 
 const signupPage = (req, res) => {
-  res.render('signup');
+  res.render('signup', { csrfToken: req.csrfToken() });
 };
 
 const logout = (req, res) => {
@@ -67,8 +67,8 @@ const signup = (request, response) => {
     const savePromise = newAccount.save();
 
     savePromise.then(() => {
-    	req.session.account = Account.AccountModel.toAPI(newAccount);
-    	return res.json({ redirect: '/maker' });
+      req.session.account = Account.AccountModel.toAPI(newAccount);
+      return res.json({ redirect: '/maker' });
     });
 
     savePromise.catch((err) => {
